e6a5d9a894efc189782e4b6be161c165742c15d3
DDoS <9475098@gmail.com>
2023-06-26 00:07:34 +0300

    update3

--- VK_foto/Run.py
+++ VK_foto/Run.py
@@ -12,23 +12,23 @@ print(f'Для скачивания доступно {len(foto.max_size_foto(use
 count_foto = int(input('Сколько фото скачать? : '))
 if __name__ == '__main__':
     count = []
-    info = []
+    list_foto = []
     for j, i in enumerate(foto.max_size_foto(user_ids)):
         if i[1]['count'] not in count:
             count.append(i[1]['count'])
             disk = ya_disk_foto.YandexDisk(user_ids, folder_name, f"{i[0]['url']}", f"{i[1]['count']}.jpg", ya_token)
             disk.upload_file_to_disk()
-            info.append(disk)
+
 
             if j + 1 == count_foto:
                 break
         else:
             disk = ya_disk_foto.YandexDisk(user_ids, folder_name, f"{i[0]['url']}", f"{i[2]}.jpg", ya_token)
             disk.upload_file_to_disk()
-
+        list_foto.append(i)
         sg.one_line_progress_meter('Загрузка', j + 1, len(foto.max_size_foto(user_ids)), '-key-')
         time.sleep(1)
 
         file = f'{user_ids}.json'
         with open(file, 'w') as f:
-            json.dump(i, f)
+            json.dump(list_foto, f)
